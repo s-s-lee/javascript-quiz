28d807b8a2495caa5bd47db39d2e017a746c8493
s-s-lee <sohzahn@gmail.com>
2022-10-05 22:36:36 -0400

    changes didnt go with last push

---
+++ assets/js/questions.js
@@ -0,0 +1,58 @@
+// collection of questions
+var questions = [
+    {
+        id: 1,
+        question: "Arrays in JavaScript can be used to store _______.",
+        answer: "All of the above",
+        options: [
+            "Numbers and strings",
+            "Other arrays",
+            "Booleans",
+            "All of the above",
+        ]
+    },
+    {
+        id: 2,
+        question: "Commonly used data types do NOT include:",
+        answer: "Alerts",
+        options: [
+            "Strings",
+            "Booleans",
+            "Alerts",
+            "Numbers",
+        ]
+    },
+    {
+        id: 3,
+        question: "A tool used during development and debugging for printing content to the debugger is:",
+        answer: "console.log",
+        options: [
+            "JavaScript",
+            "terminal/bash",
+            "for loops",
+            "console.log",
+        ]
+    },
+    {
+        id: 4,
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answer: "Quotes",
+        options: [
+            "Commas",
+            "Quotes",
+            "Parenthesis",
+            "Curly brackets",
+        ]
+    },
+    {
+        id: 5,
+        question: "The condition in an if/else statement is enclosed with ______.",
+        answer: "Parenthesis",
+        options: [
+            "Curly brackets",
+            "Quotes",
+            "Parenthesis",
+            "Square brackets",
+        ]
+    }
+];
